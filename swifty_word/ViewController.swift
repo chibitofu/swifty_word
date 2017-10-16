161a6e026017b0c191b5e03ff8bc57ec3d914174
chibitofu <erinmoony@gmail.com>
2017-10-15 23:52:26 -0700

    Added comments to UI elements.

--- swifty_word/ViewController.swift
+++ swifty_word/ViewController.swift
@@ -9,15 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Label where the clues are.
     @IBOutlet weak var cluesLabel: UILabel!
+    
+    //Label that holds the answers.
     @IBOutlet weak var answersLabel: UILabel!
+    
+    //Current score label.
     @IBOutlet weak var scoreLabel: UILabel!
+    
+    //Displays the current answer.
     @IBOutlet weak var currentAnswer: UITextField!
+    
+    //Button to submit the answer.
     @IBAction func submitTapped(_ sender: Any) {
     }
+    
+    //Button to clear the current answer.
     @IBAction func clearTapped(_ sender: Any) {
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
