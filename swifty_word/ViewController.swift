eed75a1d63e0a32c03bececd198f5363cdba012a
chibitofu <erinmoony@gmail.com>
2017-10-18 15:11:39 -0700

    Updated comments.

--- swifty_word/ViewController.swift
+++ swifty_word/ViewController.swift
@@ -16,68 +16,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var currentAnswer: UITextField!
     
+    //The 20 buttons that hold the letters.
     var letterButtons = [UIButton]()
-    var activatedButtons = [UIButton]()
-    var solutions = [String]()
     
+    //Holds all the buttons that have been pressed.
+    var activatedButtons = [UIButton]()
+    
+    //Holds all the answers.
+    var solutions = [String]()
+    var level = 1
     var score = 0 {
         didSet {
             scoreLabel.text = "Score: \(score)"
         }
-    }
-    
-    var level = 1
-    
-    func levelUp(action: UIAlertAction) {
-        level += 1
-        solutions.removeAll(keepingCapacity: true)
-        
-        loadLevel()
-        
-        for btn in letterButtons {
-            btn.isHidden = false
-        }
-    }
-    
-    @IBAction func submitTapped(_ sender: Any) {
-        if let solutionPosition = solutions.index(of: currentAnswer.text!) {
-            activatedButtons.removeAll()
-            
-            var splitAnswers = answersLabel.text!.components(separatedBy: "\n")
-            splitAnswers[solutionPosition] = currentAnswer.text!
-            answersLabel.text = splitAnswers.joined(separator: "\n")
-            
-            currentAnswer.text = ""
-            score += 1
-            
-            if score % 7 == 0 {
-                let ac = UIAlertController(title: "Well done!", message: "Are you ready for the next level?", preferredStyle: .alert)
-                ac.addAction(UIAlertAction(title: "Let's go!", style: .default, handler: levelUp))
-                ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-                present(ac, animated: true)
-            }
-        } else {
-            let ac = UIAlertController(title: "Incorrect", message: "That answer does not match any of the clues", preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: clearTapped))
-            present(ac, animated: true)
-        }
-    }
-    
-    @IBAction func clearTapped(_ sender: Any) {
-        currentAnswer.text = ""
-        
-        for btn in activatedButtons {
-            btn.isHidden = false
-        }
-        
-        activatedButtons.removeAll()
-    }
-    
-    
-    @objc func letterTapped(btn: UIButton) {
-        currentAnswer.text = currentAnswer.text! + btn.titleLabel!.text!
-        activatedButtons.append(btn)
-        btn.isHidden = true
     }
     
     func loadLevel() {
@@ -85,11 +36,14 @@ class ViewController: UIViewController {
         var solutionString = ""
         var letterBits = [String]()
         
+        //Loads file from the main directory, and splits each line up into an array item.
         if let levelFilePath = Bundle.main.path(forResource: "level\(level)", ofType: "txt") {
             if let levelContents = try? String(contentsOfFile: levelFilePath) {
                 var lines = levelContents.components(separatedBy: "\n")
+                //Randomizes each line using Gamekit methods.
                 lines = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: lines) as! [String]
-
+                
+                //Splits each line into answers and clues.
                 for (index, line) in lines.enumerated() {
                     let parts = line.components(separatedBy: ": ")
                     let answer = parts[0]
@@ -97,6 +51,7 @@ class ViewController: UIViewController {
                     
                     clueString += "\(index + 1). \(clue)\n"
                     
+                    //Takes away the "|" from the answer string and puts them into solutions array.
                     let solutionWord = answer.replacingOccurrences(of: "|", with: "")
                     solutionString += "\(solutionWord.count) letters\n"
                     solutions.append(solutionWord)
@@ -112,6 +67,7 @@ class ViewController: UIViewController {
         
         letterBits = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: letterBits) as! [String]
         
+        //Assign each letter group to a button.
         if letterBits.count == letterButtons.count {
             for i in 0 ..< letterBits.count {
                 letterButtons[i].setTitle(letterBits[i], for: .normal)
@@ -119,9 +75,63 @@ class ViewController: UIViewController {
         }
     }
     
+    func levelUp(action: UIAlertAction) {
+        level += 1
+        //keepingCapacity holds the arrays place in memory.
+        solutions.removeAll(keepingCapacity: true)
+
+        for btn in letterButtons {
+            btn.isHidden = false
+        }
+    }
+    
+    @IBAction func submitTapped(_ sender: Any) {
+        //Finds the index of the correct answer then updates the answersLabel.
+        if let solutionPosition = solutions.index(of: currentAnswer.text!) {
+            activatedButtons.removeAll()
+            
+            var splitAnswers = answersLabel.text!.components(separatedBy: "\n")
+            splitAnswers[solutionPosition] = currentAnswer.text!
+            answersLabel.text = splitAnswers.joined(separator: "\n")
+            
+            currentAnswer.text = ""
+            score += 1
+            
+            //If the player gets all 7 answers right. The level advances.
+            if score % 7 == 0 {
+                let ac = UIAlertController(title: "Well done!", message: "Are you ready for the next level?", preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "Let's go!", style: .default, handler: levelUp))
+                ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+                present(ac, animated: true)
+            }
+        } else {
+            let ac = UIAlertController(title: "Incorrect", message: "That answer does not match any of the clues", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: clearTapped))
+            present(ac, animated: true)
+        }
+    }
+    
+    //Resets the state of buttons in the current game state.
+    @IBAction func clearTapped(_ sender: Any) {
+        currentAnswer.text = ""
+        
+        for btn in activatedButtons {
+            btn.isHidden = false
+        }
+        
+        activatedButtons.removeAll()
+    }
+    
+    @objc func letterTapped(btn: UIButton) {
+        currentAnswer.text = currentAnswer.text! + btn.titleLabel!.text!
+        activatedButtons.append(btn)
+        btn.isHidden = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        //Attaches front end letter buttons to the letterTapped function.
         for subview in view.subviews where subview.tag == 1001 {
             let btn = subview as! UIButton
             letterButtons.append(btn)
